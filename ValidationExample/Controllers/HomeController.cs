d8f75a2193933caac293e83727e1eb6e2d6d88e5
Liang Wu <liangwuweb@gmail.com>
2023-05-08 11:36:46 -0500

    frombody, allow the model binding to receive json data and pass to action method param

--- ValidationExample/Controllers/HomeController.cs
+++ ValidationExample/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace ValidationExample.Controllers
     {
         [Route("register")]
         //public IActionResult Index([Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Password), nameof(Person.ConfirmPassword))] Person person)
-        public IActionResult Index(Person person)
+        public IActionResult Index([FromBody] Person person)
         {
 
             if (!ModelState.IsValid) 
